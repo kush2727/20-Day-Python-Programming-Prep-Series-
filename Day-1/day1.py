2389d5c9bda258351c24a2d6c6292c9b4682780b
kush2727 <148939040+kush2727@users.noreply.github.com>
2025-10-29 01:33:44 +0530

    Add files via upload

---
+++ Day-1/day1.py
@@ -0,0 +1,44 @@
+'''1)Write a program to check if a number is even or odd.'''
+# input=int(input("Enter Num: "))
+# if input%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+'''2)Write a program to find the largest of three numbers.'''
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# if a >= b and a >= c:
+#     print("The largest number is:", a)
+# elif b >= a and b >= c:
+#     print("The largest number is:", b)
+# else:
+#     print("The largest number is:", c)
+
+'''3)Write a program to print the sum of first n natural numbers.'''
+# n=int(input("Enter num: "))
+# sum=0
+# for i in range(1,n+1,1):
+#     sum+=i
+#
+# print(sum)
+
+'''4)Write a program to print multiplication table of a number.'''
+# n=int(input("Enter Num: "))
+# prod=1
+# for i in range(1,10+1,1):
+#     prod=n*i
+#     print(f"{n}X{i}={prod}")
+
+'''5)Write a program to check if a number is prime.'''
+# n=int(input("Enter Num: "))
+# count=0
+# for i in range(1,n+1,1):
+#     if n%i==0:
+#         count+=1
+#
+# if count==2:
+#     print("Prime")
+# else:
+#     print("Not a Prime")
